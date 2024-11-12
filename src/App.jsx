6755b645ed1d54434cfdcc6ef364d3cb691c0ce3
shaksham08 <shakshamsinha@outlook.com>
2024-11-12 22:36:22 +0530

    dev:added basic count using recoil state management

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import { useRecoilState } from "recoil";
+import { countAtom } from "./store/atoms/Count";
 
 function App() {
-  const [count, setCount] = useState(0);
-
+  const [value, setValue] = useRecoilState(countAtom);
   return (
     <>
       <div>
@@ -18,8 +18,12 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <button
+          onClick={() => {
+            setValue(value + 1);
+          }}
+        >
+          count is {value}
         </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
